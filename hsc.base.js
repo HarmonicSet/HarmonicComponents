6a03420ad0665723927f7600098592e0b3787edf
HarmonicSet <harmonicset@outlook.com>
2022-03-18 10:26:08 +0330

    First update of editor without content editabale and execCommand

--- hsc.base.js
+++ hsc.base.js
@@ -8,9 +8,18 @@ _.doc = {
         BACK_COLOR: "backColor",
         BOLD: "bold"
     },
+    CARET: "<hsc-caret></hsc-caret>",//¥
     cmd: (command, val = "") => {
         //execCommand is deprecated -> https://w3c.github.io/input-events/
         return document.execCommand(command, false, val)
+    },
+    pos: (editor) => {
+        return editor.innerHTML.indexOf(_.doc.CARET)
+    },
+    put: (doc, char) => {
+        doc.innerHTML = doc.innerHTML.replace(_.doc.CARET, "¶");
+        doc.innerHTML = doc.innerHTML.replace("¶", char + "¶");
+        doc.innerHTML = doc.innerHTML.replace("¶", _.doc.CARET);
     }
 }
 
@@ -26,16 +35,24 @@ hsc.editor = {
         _.i(holder).innerHTML = `
         <button onclick="_.doc.cmd(_.doc.TYPE.BACK_COLOR,'#21abc3')">BACK_COLOR</button>
         <button onclick="_.doc.cmd(_.doc.TYPE.BOLD)">BOLD</button>
-        <hsc-editor id=${holder}_editor contenteditable="false">
-        ABC<hsc-caret></hsc-caret>DEF
+        
+        <hsc-editor id=${holder}_editor>
+        ABC${_.doc.CARET}DEF
         </hsc-editor>
         `
         let editor = _.i(holder + "_editor");
-        editor.onkeypress = () => {
-            //¥
-            editor.innerHTML.replace("<hsc-caret></hsc-caret>","¶");
-            
-            editor.innerHTML.replace("¶","<hsc-caret></hsc-caret>");
-        }
+        document.addEventListener('keydown', (e) => {
+            if (e.key.length == 1)
+                _.doc.put(editor, e.key)
+            else {
+                if (e.key === "Enter")
+                    _.doc.put(editor, "<br/>")
+            }
+        }, false);
+
+        ele.addEventListener('keydown', function () {
+            //code
+        }, false);
+
     }
 }
