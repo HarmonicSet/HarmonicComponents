c70ee03db21a3d6e6030ec93de45f2c2c9a82c7f
HarmonicSet <harmonicset@outlook.com>
2022-03-18 08:38:34 +0330

    Update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# HarmonicComponents
+# Harmonic Components
 Some simple JavaScript components
+
+## Harmonic Com Web Text Editor
+Given that document.execCommand is being lost and deprecated. We need to find an alternative. We are writing a Web Text Editor to solve this problem.
--- hsc.base.js
+++ hsc.base.js
@@ -1,5 +1,17 @@
 let _ = {
+    g: (query) => this.querySelectorAll(query),
     i: (id) => document.getElementById(id)
+}
+
+_.doc = {
+    TYPES: {
+        BACK_COLOR: "backColor",
+        BOLD: "bold"
+    },
+    cmd: (command, val = "") => {
+        //execCommand is deprecated -> https://w3c.github.io/input-events/
+        return document.execCommand(command, false, val)
+    }
 }
 
 /**
@@ -10,11 +22,20 @@ let hsc = {
 }
 
 hsc.editor = {
-    init:(holder)=>{
-        _.i(holder).innerHTML=`
-        <hsc-editor contenteditable="true">
-        
+    init: (holder) => {
+        _.i(holder).innerHTML = `
+        <button onclick="_.doc.cmd(_.doc.TYPE.BACK_COLOR,'#21abc3')">BACK_COLOR</button>
+        <button onclick="_.doc.cmd(_.doc.TYPE.BOLD)">BOLD</button>
+        <hsc-editor id=${holder}_editor contenteditable="false">
+        ABC<hsc-caret></hsc-caret>DEF
         </hsc-editor>
         `
+        let editor = _.i(holder + "_editor");
+        editor.onkeypress = () => {
+            //¥
+            editor.innerHTML.replace("<hsc-caret></hsc-caret>","¶");
+            
+            editor.innerHTML.replace("¶","<hsc-caret></hsc-caret>");
+        }
     }
 }
